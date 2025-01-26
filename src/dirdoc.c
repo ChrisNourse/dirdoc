e03c3aece4b2985a4472d3634e1d2386eb109006
Chris Nourse <christopher.nourse@gmail.com>
2025-01-25 22:59:16 -0800

    Adding help menu

--- src/dirdoc.c
+++ src/dirdoc.c
@@ -1,14 +1,55 @@
-// dirdoc.c
 #include "cosmopolitan.h"
 #include "dirdoc.h"
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fputs("Usage: dirdoc <directory> [output.md]\n", stderr);
-    return 1;
-  }
+void print_help() {
+    printf("Usage: dirdoc [options] <directory>\n\n"
+           "Options:\n"
+           "  -h, --help     Show this help message\n"
+           "  -o, --output   Specify output file (default: directory_documentation.md)\n"
+           "\nExamples:\n"
+           "  dirdoc /path/to/dir\n"
+           "  dirdoc -o custom.md /path/to/dir\n");
+}
 
-  const char *input_dir = argv[1];
-  const char *output_file = argc > 2 ? argv[2] : NULL;
-  return document_directory(input_dir, output_file);
+int main(int argc, char *argv[]) {
+    const char *input_dir = NULL;
+    const char *output_file = NULL;
+
+    // Show help if no arguments
+    if (argc < 2) {
+        print_help();
+        return 0;
+    }
+
+    // Parse arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            print_help();
+            return 0;
+        }
+        else if ((strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output") == 0) && i + 1 < argc) {
+            output_file = argv[++i];
+        }
+        else if (argv[i][0] == '-') {
+            fprintf(stderr, "Unknown option: %s\n", argv[i]);
+            print_help();
+            return 1;
+        }
+        else {
+            if (input_dir) {
+                fprintf(stderr, "Multiple directories specified\n");
+                print_help();
+                return 1;
+            }
+            input_dir = argv[i];
+        }
+    }
+
+    if (!input_dir) {
+        fprintf(stderr, "No directory specified\n");
+        print_help();
+        return 1;
+    }
+
+    return document_directory(input_dir, output_file);
 }
